21341519f58feaa0f379bb774261efc2d0b3f319
Ivan <ivan.zderadicka@gmail.com>
2025-03-01 19:35:40 +0100

    Fix secret file permissions

--- crates/mbs4-server/src/lib.rs
+++ crates/mbs4-server/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod config;
 pub mod error;
 
+use std::path::Path;
+
 use config::ServerConfig;
 pub use error::{Error, Result};
 use mbs4_app::state::{AppConfig, AppState};
@@ -22,17 +24,38 @@ pub async fn build_state(config: &ServerConfig) -> Result<AppState> {
         .await?;
     // Its OK here to block, as it's short and called only on init;
     let data_dir = config.data_dir()?;
+    let secret = read_secret(&data_dir).await?;
+    let tokens = mbs4_auth::token::TokenManager::new(&secret, config.token_validity);
+    Ok(AppState::new(oidc_config, app_config, pool, tokens))
+}
+
+async fn read_secret(data_dir: &Path) -> Result<Vec<u8>, std::io::Error> {
     let secret_file = data_dir.join("secret");
+
     let secret = if fs::try_exists(&secret_file).await? {
         fs::read(&secret_file).await?
     } else {
         let random_bytes = rand::random::<[u8; 32]>();
-        fs::File::create(&secret_file)
-            .await?
-            .write_all(&random_bytes)
-            .await?;
+        #[cfg(unix)]
+        let mut file = {
+            use std::fs::OpenOptions;
+            use std::os::unix::fs::OpenOptionsExt;
+            {
+                // Make sure the file is only accessible by the current user
+                let _f = OpenOptions::new()
+                    .mode(0o600)
+                    .create(true)
+                    .write(true)
+                    .truncate(true)
+                    .open(&secret_file)?;
+            }
+            fs::File::options().write(true).open(&secret_file).await?
+        };
+        #[cfg(not(unix))]
+        let mut file = fs::File::create(&secret_file).await?;
+
+        file.write_all(&random_bytes).await?;
         random_bytes.as_ref().to_vec()
     };
-    let tokens = mbs4_auth::token::TokenManager::new(&secret, config.token_validity);
-    Ok(AppState::new(oidc_config, app_config, pool, tokens))
+    Ok(secret)
 }
