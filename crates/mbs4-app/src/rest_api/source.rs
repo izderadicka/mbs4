9c9c797f142720dac7fad050e649a1072eecb824
Ivan <ivan.zderadicka@gmail.com>
2025-12-25 17:59:41 +0100

    Add delete source rest API

--- crates/mbs4-app/src/rest_api/source.rs
+++ crates/mbs4-app/src/rest_api/source.rs
@@ -7,12 +7,46 @@ use crate::state::AppState;
 #[allow(unused_imports)]
 use axum::routing::{delete, get, post, put};
 
-publish_api_docs!();
+publish_api_docs!(extra_crud_api::delete_with_file);
 crud_api!(Source);
+
+mod extra_crud_api {
+    use axum::{
+        extract::{Path, State},
+        response::IntoResponse,
+    };
+    use http::StatusCode;
+    use mbs4_dal::source::SourceRepository;
+    use mbs4_store::{Store, ValidPath};
+
+    use crate::{error::ApiResult, state::AppState};
+
+    #[cfg_attr(
+        feature = "openapi",
+        utoipa::path(
+            delete,
+            path = "/{id}",
+            tag = "Source",
+            operation_id = "deleteSourceWithFile"
+        )
+    )]
+    pub async fn delete_with_file(
+        Path(id): Path<i64>,
+        repository: SourceRepository,
+        State(state): State<AppState>,
+    ) -> ApiResult<impl IntoResponse> {
+        let source = repository.get(id).await?;
+        repository.delete(id).await?;
+        let path = ValidPath::new(source.location)?.with_prefix(mbs4_store::StorePrefix::Books);
+        state.store().delete(&path).await?;
+
+        Ok((StatusCode::NO_CONTENT, ()))
+    }
+}
 
 pub fn router() -> axum::Router<AppState> {
     axum::Router::new()
-        .route("/{id}", delete(crud_api::delete))
+        .route("/{id}", delete(extra_crud_api::delete_with_file))
         .layer(RequiredRolesLayer::new([Role::Admin]))
         .route("/", post(crud_api::create))
         .route("/{id}", put(crud_api::update))
