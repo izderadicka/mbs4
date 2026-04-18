ba374be06aaec69071de356c13009b485436ac0c
Ivan <ivan.zderadicka@gmail.com>
2026-04-18 09:25:46 +0200

    Run with metrics-token

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+METRICS_TOKEN=test-token
+
 if [ -z "$1" ]; then
-    RUST_LOG=debug cargo run -p mbs4-server -- --data-dir test-data --base-url http://localhost:5173 --base-backend-url http://localhost:3000 --cors
+    RUST_LOG=debug cargo run -p mbs4-server -- --data-dir test-data --base-url http://localhost:5173 --base-backend-url http://localhost:3000 --cors --metrics-token $METRICS_TOKEN
 else 
-    RUST_LOG=debug cargo run -p mbs4-server -- --data-dir test-data --static-dir ../mbs4-client/build
+    RUST_LOG=debug cargo run -p mbs4-server -- --data-dir test-data --static-dir ../mbs4-client/build --metrics-token $METRICS_TOKEN
 fi
